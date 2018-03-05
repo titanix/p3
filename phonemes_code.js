5d9dfa85059022d62a73a563a0db7ea3afc6ef1f
Louis Lecailliez <Louis@neptun02.fi.muni.cz>
2018-03-05 19:25:17 +0100

    Données sous forme de Javascript et code de sélection à partir d'une langue.

---
+++ phonemes_code.js
@@ -0,0 +1,71 @@
+// public static String display_phoneme(Phoneme phoneme, String language, String additional_class)
+function generate_phoneme_display(phoneme, language, additional_class)
+{
+    var example = phoneme[language];
+    if(language == 'all')
+    {
+        example = '';
+    }
+
+    return String.format(
+        `<div class="phoneme_grid {2}">
+            <div class="phoneme_sound">
+                <img class="speaker" src="speaker.svg">
+            </div>
+            <div class="phoneme_symbol">{0}</div>
+            <div class="phoneme_example">{1}</div>
+        </div>`,
+        phoneme.symbol,
+        example,
+        additional_class
+    );
+}
+
+// Phoneme[] filter_phoneme_array_for_lang(Phoneme[] ph_list, String language)
+function filter_phoneme_array_for_lang(phoneme_array, language)
+{
+    var result = Array();
+    for(var p of phoneme_array)
+    {
+        if(!(p[language] === undefined))
+        {
+            result.push(p);
+        }
+    }
+    return result;
+}
+
+function selection_changed_handler()
+{
+    var select = document.getElementById('preset_language');
+
+    display_phonemes_with_selected_language(select.value);
+}
+
+function display_phonemes_with_selected_language(lang)
+{
+    var selected_phonemes = vowels;
+    document.getElementById('vowel_panel').innerHTML = "";
+
+    if(lang != 'all')
+    {
+        selected_phonemes =  filter_phoneme_array_for_lang(vowels, lang);
+        display_phonemes(selected_phonemes, lang);
+    }
+    else
+    {
+        display_phonemes(selected_phonemes, 'all');
+    }
+}
+
+function display_phonemes(phoneme_array, lang)
+{
+    var panel = document.getElementById('vowel_panel');
+    var i = 0;
+
+    for(var p of phoneme_array)
+    {
+        var css_class = i < 8 ? 'pos_1_' + (i++ + 1) : '';
+        panel.innerHTML += generate_phoneme_display(p, lang, css_class);
+    }
+}
---
+++ phonemes_data.js
@@ -0,0 +1,124 @@
+var vowels = [
+    {
+        'name' : 'close_front_unrounded_vowel',
+        'symbol' : 'i',
+        'french' : 'v<strong>i</strong>e',
+        'english' : 'b<strong>ee</strong>t',
+        'sound' : ''
+        
+    },
+    {
+        'name' : 'close_front_rounded_vowel',
+        'symbol' : 'y',
+        'french' : 'ch<strong>u</strong>te',
+        'english' : 'f<strong>ew</strong>',
+        'sound' : ''
+    },
+    {
+        'name' : 'close_central_unrounded_vowel',
+        'symbol' : 'ɨ',
+        // not in French
+        // not in American English
+        'sound' : ''
+    },
+    {
+        'name' : 'close_central_rounded_vowel',
+        'symbol' : 'ʉ',
+        // not in French
+        'english' : 'ch<strong>oo</strong>se',
+        'sound' : ''
+    },
+    {
+        'name' : 'close_back_unrounded_vowel',
+        'symbol' : 'ɯ',
+        // not in French
+        // not in American English
+        'sound' : ''
+    },
+    {
+        'name' : 'close_back_rounded_vowel',
+        'symbol' : 'u',
+        'french' : 'f<strong>ou</strong>',
+        'english' : 'b<strong>oo</strong>t',
+        'sound' : ''
+    },
+    {
+        'name' : 'near-close_near-front_unrounded_vowel',
+        'symbol' : 'ɪ',
+        // not in French
+        'english' : 'b<strong>i</strong>t',
+        'sound' : ''
+    },
+    {
+        'name' : 'near-close_near-front_rounded_vowel',
+        'symbol' : 'Y',
+        // not in French
+        // not in American English
+        'sound' : ''
+    },
+    {
+        'name' : 'near-close_near-back_rounded_vowel',
+        'symbol' : 'ʊ',
+        // not in French
+        'english' : 'h<strong>oo</strong>k',
+        'sound' : ''
+    },
+    {
+        'name' : 'close-mid_front_unrounded_vowel',
+        'symbol' : 'e',
+        'french' : 'beaut<strong>é</strong>',
+        'english' : 'pl<strong>ay</strong>',
+        'sound' : ''
+    },
+    {
+        'name' : 'close-mid_front_rounded_vowel',
+        'symbol' : 'ø',
+        'french' : 'p<strong>eu</strong>',
+        // not in American English
+        'sound' : ''
+    },
+    {
+        'name' : 'close-mid_central_unrounded_vowel',
+        'symbol' : 'ɘ',
+        // not in French
+        // not in American English
+        'sound' : ''
+    },
+    {
+        'name' : 'close-mid_central_rounded_vowel',
+        'symbol' : 'ɵ',
+        // not in French
+        // not in American English
+        'sound' : ''
+    },
+    {
+        'name' : 'close-mid_back_unrounded_vowel',
+        'symbol' : 'ɤ',
+        // not in French
+        // not in American English
+        'sound' : ''
+    },
+    {
+        'name' : 'close-mid_back_rounded_vowel',
+        'symbol' : 'o',
+        'french' : 'rés<strong>eau</strong>',
+        // not in American English ; albeit /ow/ exists
+        'sound' : ''
+    },
+    {
+        'name' : '',
+        'symbol' : '',
+        'french' : '<strong></strong>',
+        'english' : '<strong></strong>',
+        'sound' : ''
+    },
+    /*
+    {
+        'name' : '',
+        'symbol' : '',
+        'french' : '<strong></strong>',
+        'english' : '<strong></strong>',
+        'sound' : ''
+    },
+    */
+];
