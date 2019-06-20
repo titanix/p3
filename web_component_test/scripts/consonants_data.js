884089bdbd69ad96e3200b16e416e5470e6aced7
Louis Lecailliez <louis.lecailliez@outlook.fr>
2019-06-20 11:22:23 +0900

    Ajout des consonnes du français.

--- web_component_test/scripts/consonants_data.js
+++ web_component_test/scripts/consonants_data.js
@@ -8,5 +8,217 @@ var consonants = [
         'taiwanese' : '<strong>p</strong>ian 邊',
         'sound' : '',
 		'all' : ''     
-    }
+    },
+    {
+        'name' : 'voiced_bilabial_stop',
+        'symbol' : 'b',
+        'french' : '<strong>b</strong>é<strong>b</strong>é',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiceless_alveolar_plosive',
+        'symbol' : 't',
+        'french' : '<strong>t</strong>rain',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiced_alveolar_plosive',
+        'symbol' : 'd',
+        'french' : '<strong>d</strong>ent',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiceless_velar_stop',
+        'symbol' : 'k',
+        'french' : '<strong>c</strong>arton',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiced_velar_stop',
+        'symbol' : 'g',
+        'french' : '<strong>g</strong>ros',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'bilabial_nasal',
+        'symbol' : 'm',
+        'french' : '<strong>m</strong>ain',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'alveolar_nasal',
+        'symbol' : 'n',
+        'french' : '<strong>n</strong>on',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'palatal_nasal',
+        'symbol' : 'ɲ',
+        'french' : 'monta<strong>gn</strong>e',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'velar_nasal',
+        'symbol' : 'ŋ',
+        'french' : 'parki<strong>ng</strong>',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiceless_labiodental_fricative',
+        'symbol' : 'f',
+        'french' : '<strong>f</strong>eu',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiced_labiodental_fricative',
+        'symbol' : 'v',
+        'french' : '<strong>v</strong>oiture',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiceless_alveolar_sibilant',
+        'symbol' : 's',
+        'french' : '<strong>s</strong>able',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiced_alveolar_sibilant',
+        'symbol' : 'z',
+        'french' : '<strong>z</strong>oo',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiceless_palato-alveolar_sibilant',
+        'symbol' : 'ʃ',
+        'french' : '<strong>ch</strong>at',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiced_palato-alveolar_sibilant',
+        'symbol' : 'ʒ',
+        'french' : '<strong>j</strong>eu',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiced_uvular_fricative',
+        'symbol' : 'ʁ',
+        'french' : '<strong>r</strong>oi',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'alveolar_lateral_approximant',
+        'symbol' : 'l',
+        'french' : 'i<strong>l</strong>',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'palatal_approximant',
+        'symbol' : 'j',
+        'french' : 'œ<strong>il</strong>',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'labial-palatal_approximant',
+        'symbol' : 'ɥ',
+        'french' : '<strong>hu</strong>it',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    {
+        'name' : 'voiced_labio-velar_approximant',
+        'symbol' : 'w',
+        'french' : '<strong>ou</strong>i',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    /*
+    {
+        'name' : '',
+        'symbol' : '',
+        //'french' : '',
+        //'english' : '',
+        //'czech' : '',
+        //'taiwanese' : '',
+        'sound' : '',
+		'all' : ''     
+    },
+    */
 ];
